9c013072b4a3765b5456b4018d1795285ce67ecb
apintar <adampintar@gmail.com>
2017-07-13 13:41:53 -0400

    added test for fullMLE

---
+++ tests/testthat/test-fullMLE.R
@@ -0,0 +1,23 @@
+library(potMax)
+context('Full Maximum Likelihood Estimate')
+
+test_that('fullMLE produces expected result', {
+  a <- decluster(-scan('../../data/jp1tap1715wind270.csv',
+                       skip = 1, quiet = TRUE))
+  a1 <- a$declustered_series[a$declustered_series > 1]
+  mle <- fullMLE(x = a1, hessian_tf = TRUE,
+                 lt = 100, thresh = 1, n_starts = 20)
+  expect_equal(round(mle$par, 2),
+               c(1.77, 0.37, 0.19))
+  expect_equal(round(mle$hessian/100, 0)*100,
+               matrix(data = c(-30200,
+                               65300,
+                               -29900,
+                               65300,
+                               -148500,
+                               69500,
+                               -29900,
+                               69500,
+                               -33600),
+                      nrow = 3))
+})
